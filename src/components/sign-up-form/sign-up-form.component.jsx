3c4958a9ac128bc67a7b19a6bb6edbf14bf1ca72
Emmett <76142235+snakesford@users.noreply.github.com>
2024-01-02 10:52:43 -0800

    removed extra import instance

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
