6c0c3d67f4179026201bc12a8a1225a18dfd6014
Emmett <76142235+snakesford@users.noreply.github.com>
2024-01-02 13:23:52 -0800

    changes indendation and removes logo form react

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -4,12 +4,7 @@ const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category
   return (
     <div className="category-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop now</p>
