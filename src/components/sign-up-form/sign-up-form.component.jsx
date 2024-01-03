84c3942d24d65c31c3278951cd7ddc3c50c4abf2
Emmett <76142235+snakesford@users.noreply.github.com>
2024-01-03 07:51:11 -0800

    adds styling in Custom Button Component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,6 +3,8 @@ import FormInput from '../form-input/form-input.component';
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
+import './sign-up-form.styles.scss';
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -51,8 +53,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-form-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
