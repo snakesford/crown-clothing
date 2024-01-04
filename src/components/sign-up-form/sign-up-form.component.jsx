50e527c363fc9beb019683d83a76e28f3b8b297e
Emmett <76142235+snakesford@users.noreply.github.com>
2024-01-04 08:28:10 -0800

    authenticates user with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -56,7 +56,7 @@ const SignUpForm = () => {
 
   return (
     <div className="sign-up-form-container">
-      <h2>Don't have an account?</h2>
+      <h2>I do not have an account</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
