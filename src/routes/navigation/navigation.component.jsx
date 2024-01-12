3e0ee5023d4d99ded988f84d68bba984a852ef84
Emmett <76142235+snakesford@users.noreply.github.com>
2024-01-12 07:49:38 -0800

    Cart Icon Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,15 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import './navigation.styles.scss'
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../contexts/user.context';
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
+import './navigation.styles.scss'
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext)
@@ -29,7 +32,9 @@ const Navigation = () => {
                 SIGN IN
               </Link>
               )}
+              <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
